a4768e2629c9b528b889f00bb13f5e9779e90c75
Saisreegit <saisreekemburu@gmail.com>
2025-10-07 10:37:47 +0530

    dummy

--- crms/urls.py
+++ crms/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('new-request/', views.new_request, name='new_request'),
     path('new-request-step2/<str:change_request_id>/', views.new_request_step2, name='new_request_step2'),
     path('existing-change-request/', views.existing_change_request, name='existing_change_request'),
+
+
+    
     path('existing-change-request/<str:change_request_id>/', views.existing_change_request_step1, name='existing_change_request_step1'),
     path('existing-change-request-step2/<str:change_request_id>/', views.existing_change_request_step2, name='existing_change_request_step2'),
     path('download-db-file/<str:cr_id>/', views.download_db_file, name='download_db_file'),
